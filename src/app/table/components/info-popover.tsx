bef5f501d80e500ef29ef8f420433a1e36153679
zulfikar <155657912+muhammad-zulfikar@users.noreply.github.com>
2024-07-18 12:32:44 +0000

    updates

--- src/app/table/components/info-popover.tsx
+++ src/app/table/components/info-popover.tsx
@@ -59,13 +59,6 @@ export function InfoPopover({ csvInfo }: InfoPopoverProps) {
                 <Input value={emptyCells.toString()} readOnly className="h-8 bg-transparent text-right w-full" />
               </div>
             </div>
-            
-            <div className="grid grid-cols-2 items-center gap-4">
-              <Label>Column Names</Label>
-              <div className="text-right">
-                <Input value={columnNames} readOnly className="h-8 bg-transparent text-right w-full text-ellipsis" />
-              </div>
-            </div>
             <div className="grid grid-cols-2 items-center gap-4">
               <Label>Delimiter</Label>
               <div className="text-right">
